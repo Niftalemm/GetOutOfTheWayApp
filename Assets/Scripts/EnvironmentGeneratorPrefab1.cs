9821a7b31ba5eaee88d1b3b1321a57290244da53
Skeeter Cathcart <david.cathcart@mnsu.edu>
2023-03-29 15:13:05 -0500

    "Pushing for Karl"

--- Assets/Scripts/EnvironmentGeneratorPrefab1.cs
+++ Assets/Scripts/EnvironmentGeneratorPrefab1.cs
@@ -8,10 +8,31 @@ using UnityEditor.Experimental.GraphView;
 
 public class EnvironmentGeneratorPrefab1 : MonoBehaviour
 {
+   
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-     
+
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+        // Adding a Tag
+        string[] s = { "Pillar", "Ball", "Wall", "Goal" };
+
+        for (int i = 0; i < s.Length; i++)
+        {
+           
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty n = tagsProp.GetArrayElementAtIndex(0);
+            n.stringValue = s[i];
+            tagManager.ApplyModifiedPropertiesWithoutUndo();
+        }
+        
+
+
         /* Hardcoded pillar coordinates */
         Vector3[] PillarCords = { new Vector3(76, 7, 77), new Vector3(-40, 7, 61), new Vector3(3, 7, -58), new Vector3(50, 7, 17), new Vector3(-76, 7, -26),
         new Vector3(54, 7, -61), new Vector3(-9, 7, -3), new Vector3(10, 7, 58)};
@@ -20,7 +41,7 @@ public class EnvironmentGeneratorPrefab1 : MonoBehaviour
         Vector3[] WallCords = { new Vector3(0, 15, -100), new Vector3(100, 15, 0), new Vector3(-100, 15, 0), new Vector3(0, 15, 100) };
 
         /* Hardcoded wall orientation */
-        Vector3[] WallOrient = { new Vector3(90, 0, 0), new Vector3(90, 0, 90), new Vector3(270, 0, 270), new Vector3(270, 0, 0) };
+        Vector3[] WallOrient = { new Vector3(90, 0, 0), new Vector3(90, 0, 90), new Vector3(90, 0, 90), new Vector3(90, 0, 0) };
 
         Vector3 WallScale = new Vector3(20, 1, 3);
         Vector3 PillarScale = new Vector3(5, 15, 5);
@@ -38,13 +59,15 @@ public class EnvironmentGeneratorPrefab1 : MonoBehaviour
             pillar.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
             pillar.AddComponent<AkGameObj>();
             pillar.AddComponent<AkAmbient>();
-            pillar.GetComponent<MeshRenderer>().material.color = Color.gray;
+            pillar.tag = "Pillar";
             AkSoundEngine.PostEvent("Play_The_Creature_1", pillar);
             pillar.transform.localScale = PillarScale;
             pillar.transform.localPosition = PillarCords[i];
             Pillars[i] = pillar;
         }
 
+
+        
         /* Generates a new wall, using each set of coordinates from the WallCords array, and rotation from the WallOrient array */
         for (int i = 0; i < WallCords.Length; i++)
         {
@@ -55,6 +78,7 @@ public class EnvironmentGeneratorPrefab1 : MonoBehaviour
             Wall.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
             Wall.AddComponent<AkGameObj>();
             Wall.AddComponent<AkAmbient>();
+            Wall.tag = "Wall";
             AkSoundEngine.PostEvent("Play_The_Creature_1", Wall);
             Wall.transform.localScale = WallScale;
             Wall.transform.localPosition = WallCords[i];
@@ -66,16 +90,16 @@ public class EnvironmentGeneratorPrefab1 : MonoBehaviour
         plane1.transform.localScale = new Vector3(20, 1, 20);
 
         GameObject goal = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Destroy(goal.GetComponent<BoxCollider>());
         goal.AddComponent<Rigidbody>();
         goal.GetComponent<Rigidbody>().useGravity = false;
         goal.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
         goal.AddComponent<AkGameObj>();
         goal.AddComponent<AkAmbient>();
+        AkSoundEngine.PostEvent("Play_MUSCChim_InsJ_Hand_Bell_01_16_High_C", goal);
         goal.transform.localScale = new Vector3(8, 10, 8);
         goal.transform.localPosition = new Vector3(-5, 5, 79);
-        goal.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
-        AkSoundEngine.PostEvent("Play_MUSCChim_InsJ_Hand_Bell_01_16_High_C", goal);
+        goal.GetComponent<Renderer>().material.color = new Color(34, 139, 34);
+        goal.tag = "Goal";
     }
     // Update is called once per frame
     void Update()
