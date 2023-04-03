97e1acfb2b59153b72c74e4db6fa2308e6ddc942
Karl Betcher <karl.betcher@mnsu.edu>
2023-04-03 15:34:28 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    gReAt cOmMiT sUmMaRiEs cOnTaIn LeSs tHaN 50 cHaRaCtErS
    
    todo:
    1. script the functionality of it
         - Input.GetKeyDown has never been so difficult or mentally taxing
         - Also what even is a Phsyics.Raycast
         - Set PlayerPosition in GoalBehavior Prefab
    2. Add each sound to soundbank for the following objects
    - Walls (low hum)
    - Balls (car engine)
    - Goal
    - Pillars
    - Obstacle Collision Sound
    - Wall collision sound
    - Ball collision sound
    - Goal completion sound
    - Walking sound (maybe)
    3. Add sound and attenuation to the following objects:
    - Walls (post a low, highly attenuated hum)
    - Pillars (post a sound, remember what the sound was)
    - Goal (post goal ping sound)
    - Ball (post engine sound)
    - FirstPersonController (get it to listen)
    4. Build Tutorial Levels
    5. Add bird's-eye view camera, separate from listener object & first person controller
    6. Add random level generation for the remaining levels.
    7. Fix issues
    - walls on +-x in Envionrment Prefab have a large box where you can't walk towards it
    - balls deflect on hit with player
    - ball script editor always defaults to 0 between runs

--- Assets/Scripts/GoalBehavior.cs
+++ Assets/Scripts/GoalBehavior.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class GoalBehavior : MonoBehaviour
 {
     public Transform playerPosition;
-    public float activationDistance = 5f;
-    bool active = true;
+    public float activationDistance = 20f;
+    private bool active = true;
+    private Material mat;
+    public Material completedMaterial;
     
     
 
@@ -14,19 +20,80 @@ public class GoalBehavior : MonoBehaviour
     void Start()
     {
         // define playerPosition value
+        // define thegameObject
+        mat = GetComponent<Material>();
+        Debug.Log(activationDistance);
+        Debug.Log("R" + completedMaterial.color.r + " G" + completedMaterial.color.g + " B" + completedMaterial.color.b);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // none of this shit is tested
+        Debug.Log("update");
+
+        // none of this shit is tested (evidently it doesn't work either)
         RaycastHit hit;
 
         if (active)
         {
             // whatever the goal does goes here
-
+            
             active = !(Physics.Raycast(playerPosition.position, playerPosition.forward, out hit, activationDistance) && Input.GetKeyDown(KeyCode.E)); // deactivate when player is looking at it and presses E
+            if (!active)
+            {
+                mat = completedMaterial;
+                Debug.Log("Goal has been pressed.");
+            }
+        }
+        if (Physics.Raycast(playerPosition.position, playerPosition.forward, out hit, activationDistance))
+        {
+            Debug.Log("Goal is being looked at.");
+        }
+        if (Input.GetKeyDown(KeyCode.E) == true)
+        {
+            Debug.Log("The E button has been pressed.");
         }
     }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(GoalBehavior)), InitializeOnLoadAttribute]
+public class GoalBehaviorEditor : Editor
+{
+    GoalBehavior gb;
+    SerializedObject SerGB;
+    Material tempMat;
+
+    private void OnEnable()
+    {
+        gb = (GoalBehavior) target;
+        SerGB = new SerializedObject(gb);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        SerGB.Update();
+        GUILayout.Label("Goal Behavior Parameters", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
+        EditorGUILayout.Space();
+
+        // create a slider for the activation distance
+        gb.activationDistance = EditorGUILayout.Slider(new GUIContent("Activation Distance", "How far away the player have to be to complete the goal"), gb.activationDistance, 5, 100, GUILayout.Height(25));
+
+        // create a ObjectField for the material
+        GUILayout.Label("Goal Completion Material", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleLeft, fontSize = 12});
+        tempMat = (Material) EditorGUILayout.ObjectField(tempMat, typeof(Material), false, GUILayout.Height(25));
+
+        gb.completedMaterial = tempMat;
+
+        // update the values of the object
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(gb);
+            Undo.RecordObject(gb, "BallInitialVelocity Change");
+            SerGB.ApplyModifiedProperties();
+        }
+    }
+}
+#endif
+
+
