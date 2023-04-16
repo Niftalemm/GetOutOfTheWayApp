5833b77040b9b76862eb71c1b5bef3f6d73933c1
Skeeter Cathcart <skeetercathcart@gmail.com>
2023-04-15 20:58:35 -0500

    Comments added to CollisionTracker.cs. The scene name and timestamps for collisions are now tracked

--- Assets/Scripts/CollisionTracker.cs
+++ Assets/Scripts/CollisionTracker.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Globalization;
 using System.IO;
 using System.Text;
+using UnityEngine.SceneManagement;
 
 
 
@@ -16,6 +17,8 @@ public class CollisionTracker : MonoBehaviour
     {
         public int staticCollisions = 0;
         public int movingCollisions = 0;
+        public List<double> staticCollisionTimes = new List<double>();
+        public List<double> movingCollisionTimes = new List<double>();  
         public List<string> position = new List<string>();
         public List<string> rotation = new List<string>();
         public float baseTimer;
@@ -26,35 +29,86 @@ public class CollisionTracker : MonoBehaviour
 
     collectedData tracker = new collectedData();
     
+    /// <summary>
+    /// This function writes data that has been gathered into a collectedData object to a .csv file. This data should only be written once per scene/level. 
+    /// I was not successful in getting the CSVHelper plugin to work. Would recommend looking into it to help with this. 
+    /// </summary>
     private void WriteToCSV()
     {
         string filePath = Application.dataPath + "/results.csv";
-        if (!File.Exists(filePath))
-        {
-            StreamWriter creativeWriter = new StreamWriter(filePath);
-            creativeWriter.WriteLine("Participant,Static Collisions,Moving Collisions,Location,Rotation,TTT1,TTT2,TTT3");
-            creativeWriter.Flush();
-            creativeWriter.Close();
-        }
         
+        StreamWriter creativeWriter = new StreamWriter(filePath);
+        creativeWriter.WriteLine("Participant,Scene,# Static Collisions,Static Timestamps,# Moving Collisions,Moving Timestamps,Location,Rotation,TTT1,TTT2,TTT3"); // Header
+        creativeWriter.Flush();
+        creativeWriter.Close();
+        
+        // List<> is converted to array for easier loops. 
         string[] tempPosition = tracker.position.ToArray();
         string[] tempRotation = tracker.rotation.ToArray();
+        double[] tempStatic = tracker.staticCollisionTimes.ToArray();
+        double[] tempMoving = tracker.movingCollisionTimes.ToArray();
+        string fullStatic = "[";
+        string fullMoving = "[";
         string fullPosition = "";
         string fullRotation = "";
+        string currentScene = SceneManager.GetActiveScene().name;
 
-        for (int i = 0; i < tempPosition.Length - 2; i++)
-        {
-            fullPosition += tempPosition[i] + ", ";
-        }
-        fullPosition += tempPosition[tempPosition.Length - 1];
+        // For every List<> (now an array), iterate through and convert to a single string
 
-        for (int i = 0; i < tempRotation.Length - 2; i++)
+        for (int i = 0; i < tempStatic.Length; i++)
         {
-            fullRotation += tempRotation[i] + ", ";
+            if (i == tempStatic.Length - 1)
+            {
+                fullStatic += tempStatic[i].ToString();
+            }
+            else
+            {
+                fullStatic += tempStatic[i].ToString() + ", ";
+            }
         }
-        fullRotation += tempRotation[tempRotation.Length - 1];
+        fullStatic += "]";
+
+        for (int i = 0; i < tempMoving.Length; i++)
+        {
+            if (i == tempMoving.Length - 1)
+            {
+                fullMoving += tempMoving[i].ToString();
+            }
+            else
+            {
+                fullMoving += tempMoving[i].ToString() + ", ";
+            }
+        }
+        fullMoving += "]";
+
+        for (int i = 0; i < tempPosition.Length; i++)
+        {
+            if (i == tempPosition.Length - 1)
+            {
+                fullPosition += tempPosition[i];
+            }
+            else
+            {
+                fullPosition += tempPosition[i] + ", ";
+            }
+        }
+        
+
+        for (int i = 0; i < tempRotation.Length; i++)
+        {
+            if (i == tempRotation.Length - 1)
+            {
+                fullRotation += tempRotation[i];
+            }
+            else
+            {
+                fullRotation += tempRotation[i] + ", ";
+            }
+        }
+        
+        //This will append to the CSV file, creating a new row, hence why WriteCSV should be called once, at the end of a level
         StreamWriter writer = File.AppendText(filePath);
-        writer.WriteLine("Goober McGee" + "," + tracker.staticCollisions + "," + tracker.movingCollisions + "," + "\"" + fullPosition + "\"" + "," + "\"" + fullRotation + "\"" + 
+        writer.WriteLine("Goober McGee" + "," + currentScene + "," + tracker.staticCollisions + "," + "\"" + fullStatic + "\"" + "," + tracker.movingCollisions + "," + "\"" + fullMoving + "\"" + "," + "\"" + fullPosition + "\"" + "," + "\"" + fullRotation + "\"" + 
             "," + tracker.timer1 + "," + tracker.timer2 + "," + tracker.timer3);
         writer.Flush();
         writer.Close();
@@ -67,7 +121,7 @@ public class CollisionTracker : MonoBehaviour
 
     private void Update()
     {
-       
+       // Cumulative time between each frame, calculated each frame, for total run time of a level
         tracker.baseTimer += Time.deltaTime;
 
     }
@@ -78,30 +132,36 @@ public class CollisionTracker : MonoBehaviour
         GameObject followMe = GameObject.Find("FirstPersonController");
         GameObject rotationRef = GameObject.Find("CrosshairAndStamina");
         // Convert Vector3 into a string resembling an array element to later convert into an array
-        string convertedPosition = ("[" + followMe.transform.position.x + "," + followMe.transform.position.y + "," + followMe.transform.position.z + "]");
-        string convertedRotation = ("[" + rotationRef.transform.rotation.x + "," + rotationRef.transform.rotation.y + "," + rotationRef.transform.rotation.z + "]");
+        string convertedPosition = ("[" + followMe.transform.position.x + "|" + followMe.transform.position.y + "|" + followMe.transform.position.z + "]");
+        string convertedRotation = ("[" + rotationRef.transform.rotation.x + "|" + rotationRef.transform.rotation.y + "|" + rotationRef.transform.rotation.z + "]");
         tracker.position.Add(convertedPosition);
         tracker.rotation.Add(convertedRotation);
     }
 
-
+    /// <summary>
+    /// Checks the object tag of the object collided with and takes action (collects certain data) based on which object was hit
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnCollisionEnter(Collision collision)
     {
 
-        if (collision.gameObject.tag == "Pillar")
+        if (collision.gameObject.tag == "Pillar" || collision.gameObject.tag == "Wall")
         {
             tracker.staticCollisions += 1;
+            tracker.staticCollisionTimes.Add(tracker.baseTimer);
+            WriteToCSV();
             Debug.Log("Pillar hit " + tracker.staticCollisions);
-        }
-        else if (collision.gameObject.tag == "Wall")
-        {
-            tracker.staticCollisions += 1;
-            Debug.Log("Wall hit " + tracker.staticCollisions);
         }
         else if (collision.gameObject.tag == "Ball")
         {
             tracker.movingCollisions += 1;
+            tracker.movingCollisionTimes.Add(tracker.baseTimer);
             Debug.Log("Ball hit " + tracker.movingCollisions);
+        }
+        else if (collision.gameObject.tag == "Goal")
+        {
+            tracker.timer1 = tracker.baseTimer;
+            Debug.Log("Goal At " + tracker.timer1);
         }
         else if (collision.gameObject.tag == "Goal1")
         {
