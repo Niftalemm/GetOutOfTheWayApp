45ff2b58d8868c409132d1517d0f3177777a856a
Wen71 <wen.sun@mnsu.edu>
2023-04-17 22:30:52 -0500

    Space keyboard done for switching levels

--- Assets/Scripts/OOF.cs
+++ Assets/Scripts/OOF.cs
@@ -1,3 +1,7 @@
+/// <summary>
+/// This script handles various game mechanics, such as collision detection and level progression. It is attached to the player object (First Person controller).
+/// </summary>
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +13,6 @@ using System.Linq;
 using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 using UnityEngine.SceneManagement;
 
-// remove goals list
-// Actvate goal
-// make sure goals get activated
 
 public class OOF : MonoBehaviour
 {
@@ -26,7 +27,8 @@ public class OOF : MonoBehaviour
     private Rigidbody rb;
     public string[] tutorialLevels = { "Level1", "Level2", "Level3" };
     public string[] levelNames = { "Level4", "Level5", "Level6", "Level7", "Level8", "Level9", "Level10", "Level11" };
-    public List<int> selectedLevels;
+    public List<int> selectedLevels; // A list of integers that correspond to the indices of selected levels
+    private bool readyForNextLevel; // flag indicating player is ready for next level
 
 
     private void Start()
@@ -34,6 +36,7 @@ public class OOF : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         goals = GameObject.FindObjectsOfType<GoalBehavior>().ToList<GoalBehavior>();
         selectedLevels = new List<int>(); // Initialize selectedLevels to an empty list
+        readyForNextLevel = false;
 
         // Load the selectedLevels list from PlayerPrefs
         if (PlayerPrefs.HasKey("SelectedLevels"))
@@ -43,23 +46,35 @@ public class OOF : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Called when the player collides with a trigger
+    /// </summary>
+    /// <param name="other">The Collider component of the object that the player collided with</param>
     private void OnTriggerEnter(Collider other)
     {
         akThud.Post(gameObject);
-        ActivateGoal(other);
-   
+        
+        //If the player does not finished all 24 trials, activate goals
         if (selectedLevels.Count < 24)
         {
             Debug.Log(selectedLevels.Count);
+            ActivateGoal(other);
+
         }
+        // If all levels have been completed, go to the success screen and reset the level
         else
         {
             Debug.Log("Game Over");
             SceneManager.LoadScene("Success");
             resetLevel();
         }
+       
     }
 
+    /// <summary>
+    /// Called when the player collides with an object with a Collider component (Pillar, ball and wall) attached
+    /// </summary>
+    /// <param name="collision">The Collision object that describes the collision</param>
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Pillar" || collision.gameObject.tag == "Ball")
@@ -71,27 +86,22 @@ public class OOF : MonoBehaviour
             akThud.Post(gameObject);
         }
     }
-    
+
+    /// <summary>
+    /// Called once per frame
     private void Update()
     {
-        // this probably could stand to have the input thingy removed but we do not have time
-        // maybe even remove this method but that's messing with other people's code and I dont have time to deal with if it gets messed up
-        if (Input.GetKeyDown(KeyCode.E))
+        // If the game is ready to move on to the next level and the player presses spacebar, call LevelComplete() which goes to next level
+        if (readyForNextLevel && Input.GetKeyDown(KeyCode.Space))
         {
-            if (selectedLevels.Count < 24)
-            {
-                Debug.Log(selectedLevels.Count);
-            }
-            else
-            {
-                Debug.Log("Game Over");
-                SceneManager.LoadScene("Success");
-                resetLevel();
-            }
+            LevelComplete();
+            Time.timeScale = 1;// Unfreeze everything in the game
+            readyForNextLevel = false;
+
 
         }
-    }
 
+    }
     /// <summary>
     /// Activate the goal recently collided with
     /// </summary>
@@ -110,46 +120,55 @@ public class OOF : MonoBehaviour
             {
                 akLevelSuccess.Post(gameObject);
 
-                // check current level
-                Scene currentScene = SceneManager.GetActiveScene();
-                Debug.Log(currentScene.name);
-                // If the current level is tutorial scene
-                if (tutorialLevels.Contains(currentScene.name))
-                {
-                    int nextSceneIndex = System.Array.IndexOf(tutorialLevels, currentScene.name) + 1;
-                    if (nextSceneIndex < tutorialLevels.Length)
-                    {
-                        // go to next tutorial level
-                        SceneManager.LoadScene(tutorialLevels[nextSceneIndex]);
-                    }
-                    else
-                    {
-                        selectRandomLevel();
-                    }
-                }
-                else
-                {
-                    selectRandomLevel();
-                }
+                readyForNextLevel = true;
+                Time.timeScale = 0; // Freeze everything in the game
+
             }
             else
             {
                 akGoalSuccess.Post(gameObject);
             }
-
-
         }
+    }
+    /// <summary>
+    /// This method is called when a level is completed successfully. It triggers an AK Event that plays a sound, and loads the next level.
+    /// </summary>
+    private void LevelComplete()
+    {
 
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // If the current scene is a tutorial level
+        if (tutorialLevels.Contains(currentScene.name))
+        {
+            // Find the index of the next tutorial level in the tutorialLevels array
+            int nextSceneIndex = System.Array.IndexOf(tutorialLevels, currentScene.name) + 1;
+            
+            // If there is a next tutorial level, load it
+            if (nextSceneIndex < tutorialLevels.Length)
+            {
+                SceneManager.LoadScene(tutorialLevels[nextSceneIndex]);
+            }
+            // If there are no more tutorial levels, select a random level
+            else
+            {
+                SelectRandomLevel();
+            }
+        }
+        // If the current scene is not a tutorial level, select a random level
+        else
+        {
+            SelectRandomLevel();
+        }
     }
 
     /// <summary>
-    /// Randomly generate a level, and store level name into a list
+    /// Randomly selects a level from the levelNames array and loads it three times. The selected level is stored in a list to avoid selecting it again.
     /// </summary>
-    private void selectRandomLevel()
+    private void SelectRandomLevel()
     {
-        // load a random level
         int nextLevel;
+        // Loop until a level is found that has not been played three times yet
         if (selectedLevels.Count < levelNames.Length * 3)
         {
             do
@@ -157,15 +176,21 @@ public class OOF : MonoBehaviour
                 nextLevel = Random.Range(0, levelNames.Length);
             } while (selectedLevels.Count(level => level == nextLevel) >= 3);
 
+            // Add the selected level to the list of played levels
             selectedLevels.Add(nextLevel);
+
             // save selectedLevels to PlayerPrefs
             PlayerPrefs.SetString("SelectedLevels", string.Join(",", selectedLevels));
             PlayerPrefs.Save();
+
+            // Load the selected level
             SceneManager.LoadScene(levelNames[nextLevel]);
         }
     }
 
-
+    /// <summary>
+    /// Initializes the selectedLevels list to an empty list and saves it to PlayerPrefs.
+    /// </summary>
     private void resetLevel()
     {
         selectedLevels = new List<int>(); // Initialize selectedLevels to an empty list
